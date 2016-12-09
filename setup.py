1d6934b861b4c2602e095a598c9b3c49fd93c38a
Vladimir Magamedov <vladimir@magamedov.com>
2016-12-09 17:28:55 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MetricsLog',
+    version='0.1.0',
+    description='Send metrics as log',
+    author='Vladimir Magamedov',
+    author_email='vladimir@magamedov.com',
+    url='https://github.com/vmagamedov/metricslog',
+    packages=find_packages(),
+    license='BSD',
+    install_requires=[],
+)
